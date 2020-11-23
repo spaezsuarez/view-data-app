ceca85b7cad32bcbfae758a43b0cd463af6d115d
spaezsuarez <spaezsuarez@gmail.com>
2020-11-23 10:09:35 -0500

    Refactor -> Eliminacion rutas innecesarias en App.py

--- src/App.py
+++ src/App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 from routes import dataRoute, formRoute
 
 app = Flask(__name__)
@@ -7,44 +7,10 @@ app = Flask(__name__)
 app.register_blueprint(dataRoute)
 app.register_blueprint(formRoute)
 
+#Ruta por defecto
 @app.route('/')
 def main():
     return render_template('index.html')
-
-@app.route('/first-form', methods=['GET'])
-def load_first_form():
-    return redirect('/form/1')
-
-
-@app.route('/second-form', methods=['GET'])
-def load_second_form():
-    return redirect('/form/2')
-
-
-@app.route('/third-form', methods=['GET'])
-def load_third_form():
-    return redirect('/form/3')
-
-
-@app.route('/fourth-form', methods=['GET'])
-def load_fourth_form():
-    return redirect('/form/4')
-
-
-@app.route('/fifth-form', methods=['GET'])
-def load_fifth_form():
-    return redirect('/form/5')
-
-
-@app.route('/sixth-form',methods=['GET'])
-def load_sixth_form():
-    return redirect('/form/6')
-    
-
-@app.route('/seventh-form',methods=['GET'])
-def load_seventh_form():
-    return redirect('/form/7')
-
 
 if __name__ == "__main__":
     app.run(port = 3000, debug = True)
